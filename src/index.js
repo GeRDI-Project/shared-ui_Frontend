8b870c74447d0879f9c974f3d9d2b71eab00bcca
babatech <shahabi1209@gmail.com>
2018-06-23 16:20:23 +0200

    external module test 3 + one more component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import StaticFootr from './Footer.vue'
+import StaticNavMenu from './NavMenu.vue'
 
-const previewComponent =  {
+const StaticComponent =  {
     install: function (Vue, options) {
         Vue.component('static-footer', StaticFootr)
+        Vue.component('static-navMenu', StaticNavMenu)
     }
 }
 
-export default previewComponent;
+export default StaticComponent;
