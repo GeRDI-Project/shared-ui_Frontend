333bfbe69a56e913a64604ebf9acada25cb01162
Shahab Shakoor <shahabi1209@gmail.com>
2018-08-13 01:13:50 +0200

    GeDRI static components  (3 vue component in common  )

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import AppFooter from './AppFooter.vue'
 import DocumentMedia from './DocumentMedia.vue'
-import NavMenu from './NavMenu.vue'
 import PaginationBar from './PaginationBar.vue'
 
 const StaticComponent =  {
     install: function (Vue, options) {
         Vue.component('app-footer', AppFooter)
         Vue.component('document-media', DocumentMedia)
-        Vue.component('nav-menu', NavMenu)
         Vue.component('paginationBar', PaginationBar)
     }
 }
