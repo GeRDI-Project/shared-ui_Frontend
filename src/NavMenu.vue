b61b3988d33896db17a4cca496d4f0af04faa2f5
Anastasia Kazakova <a.kazakova@zbw.eu>
2019-01-29 12:48:51 +0100

    alpha logo inserted

--- src/NavMenu.vue
+++ src/NavMenu.vue
@@ -2,7 +2,7 @@
   <b-navbar toggleable="md" variant="faded" type="light">
   <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
   <b-navbar-brand href="#">
-    <img src="./assets/logo.svg" id="logo" height="70px" class="d-inline-block align-top" alt="BV">
+    <img src="./assets/logo_alpha-01.svg" id="logo" height="70px" class="d-inline-block align-top" alt="BV">
   </b-navbar-brand>
     <b-collapse is-nav id="nav_collapse">
       <b-navbar-nav>
